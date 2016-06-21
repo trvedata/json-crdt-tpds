c8bbcba20622fe3053a44fb5ede7e9da07b48c13
Martin Kleppmann <martin@kleppmann.com>
2016-06-21 21:28:14 +0100

    Use built-in list construction
    Defining our own linked list construction prevented the termination
    checker from verifying get_next, and I couldn't see how one would prove
    termination manually.

--- TrveSync.thy
+++ TrveSync.thy
@@ -19,9 +19,7 @@ datatype id =
 datatype state_key =
   MapT  id |
   ListT id |
-  RegT  id |
-  NextT id |
-  Pres  id
+  RegT  id
 
 datatype primitive =
   Null       |
@@ -32,7 +30,7 @@ datatype primitive =
 datatype doc_state =
   Prim primitive |
   IdRef id |
-  Ctx "state_key \<Rightarrow> doc_state option"
+  Ctx "(state_key * id set * doc_state) list"
 
 datatype cursor = Cursor "state_key list" id
 
@@ -48,34 +46,47 @@ fun let_var :: "state \<Rightarrow> string \<Rightarrow> cursor \<Rightarrow> st
 inductive valid_cur :: "doc_state \<Rightarrow> cursor \<Rightarrow> bool" where
 cur1: "valid_cur (Ctx ctx) (Cursor [] elem)"
 
-fun is_present :: "doc_state \<Rightarrow> id \<Rightarrow> bool" where
-"is_present (Ctx ctx) item = (
-  case (ctx (Pres item)) of
-    (Some (Ctx ops)) \<Rightarrow> (dom ops \<noteq> {}) |
-    (Some _) \<Rightarrow> False |
-    None \<Rightarrow> False
-)" |
-"is_present _ _ = False"
+fun is_present :: "doc_state \<Rightarrow> state_key \<Rightarrow> bool" where
+  "is_present (Ctx ctx) item = (
+    case (find (\<lambda>(key,_,_). key = item) ctx) of
+      (Some (key,pres,_)) \<Rightarrow> (pres \<noteq> {}) |
+      None \<Rightarrow> False
+  )"
+| "is_present _ _ = False"
 
-function get_next :: "doc_state \<Rightarrow> cursor \<Rightarrow> cursor option" where
-"get_next (Ctx ctx) (Cursor [] prev_id) = (
-  case (ctx (NextT prev_id)) of
-    (Some (IdRef Tail)) \<Rightarrow> None |
-    (Some (IdRef next_id)) \<Rightarrow> (
-      if (is_present (Ctx ctx) next_id)
-      then (Some (Cursor [] next_id))
-      else (get_next (Ctx ctx) (Cursor [] next_id))) |
-    (Some _) \<Rightarrow> None |
-    None \<Rightarrow> None)" |
-"get_next (Ctx ctx) (Cursor (k#ks) kn) = (
-  case (ctx k) of
-    (Some inner) \<Rightarrow> (
-      case (get_next inner (Cursor ks kn)) of
-        (Some (Cursor ks1 kn1)) \<Rightarrow> (Some (Cursor (k#ks1) kn1)) |
-        None \<Rightarrow> None) |
-    None \<Rightarrow> None)" |
+fun get_id :: "state_key \<Rightarrow> id" where
+  "get_id (MapT  x) = x"
+| "get_id (ListT x) = x"
+| "get_id (RegT  x) = x"
+
+fun get_first_diff :: "(state_key * id set * doc_state) list \<Rightarrow> id \<Rightarrow> id option" where
+  "get_first_diff ((key, pres, _) # rest) prev_id = (
+    if (get_id key) = prev_id \<or> pres = {}
+    then get_first_diff rest prev_id
+    else Some (get_id key)
+  )"
+| "get_first_diff [] _ = None"
+
+fun get_next :: "doc_state \<Rightarrow> cursor \<Rightarrow> cursor option" where
+  "get_next (Ctx ((key, pres, child) # rest)) (Cursor [] prev_id) = (
+    if (get_id key) \<noteq> prev_id
+    then get_next (Ctx rest) (Cursor [] prev_id)
+    else (
+      case (get_first_diff rest prev_id) of
+        Some(next_id) \<Rightarrow> (Some (Cursor [] next_id))
+      | None \<Rightarrow> None
+    )
+  )"
+| "get_next (Ctx ((key, pres, child) # rest)) (Cursor (k#ks) kn) = (
+    if key \<noteq> k
+    then get_next (Ctx rest) (Cursor (k#ks) kn)
+    else (
+      case (get_next child (Cursor ks kn)) of
+        (Some (Cursor ks1 kn1)) \<Rightarrow> (Some (Cursor (k#ks1) kn1))
+      | None \<Rightarrow> None
+    )
+  )" |
 "get_next _ _ = None"
-by pat_completeness auto
 
 fun eval_cur :: "state \<Rightarrow> expr \<Rightarrow> cursor option" where
 "eval_cur _ Doc = (Some (Cursor [] Root))" |
@@ -101,7 +112,7 @@ expr_iter: "valid_expr state expr \<Longrightarrow> valid_expr state (Iter expr)
 expr_next: "valid_expr state expr \<Longrightarrow> valid_next state expr \<Longrightarrow> valid_expr state (Next expr)"
 
 definition empty_state :: state
-where "empty_state = (State (Ctx Map.empty) Map.empty)"
+where "empty_state = (State (Ctx []) Map.empty)"
 
 lemma "valid_expr empty_state Doc"
 by (rule expr_doc)
