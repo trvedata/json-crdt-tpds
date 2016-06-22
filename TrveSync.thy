7e161ad872f139439523eb85d08d9fd5ce6d6770
Martin Kleppmann <martin@kleppmann.com>
2016-06-22 15:36:01 +0100

    Use record type for state

--- TrveSync.thy
+++ TrveSync.thy
@@ -33,35 +33,39 @@ datatype doc_state =
 
 datatype cursor = Cursor "state_key list" id
 
-datatype state =
-  State doc_state "string \<Rightarrow> cursor option"
+record state =
+  doc :: doc_state
+  vars :: "string \<Rightarrow> cursor option"
+
 
 fun defined_var :: "string \<Rightarrow> state \<Rightarrow> bool" where
-"defined_var x (State _ vars) = (x \<in> dom vars)"
+  "defined_var x state =
+    (x \<in> dom (vars state))"
 
 fun let_var :: "state \<Rightarrow> string \<Rightarrow> cursor \<Rightarrow> state" where
-"let_var (State doc vars) key val = State doc (vars(key \<mapsto> val))"
+  "let_var state key val =
+    state \<lparr>vars := (vars state)(key \<mapsto> val)\<rparr>"
 
 fun get_id :: "state_key \<Rightarrow> id" where
   "get_id (MapT  x) = x"
 | "get_id (ListT x) = x"
 | "get_id (RegT  x) = x"
 
-fun get_first_diff :: "(state_key * id set * doc_state) list \<Rightarrow> id \<Rightarrow> id option" where
-  "get_first_diff ((key, pres, _) # rest) prev_id = (
-    if (get_id key) = prev_id \<or> pres = {}
-    then get_first_diff rest prev_id
+fun first_present :: "(state_key * id set * doc_state) list \<Rightarrow> id option" where
+  "first_present ((key, pres, _) # rest) = (
+    if pres = {}
+    then first_present rest
     else Some (get_id key)
   )"
-| "get_first_diff [] _ = None"
+| "first_present [] = None"
 
 fun get_next :: "doc_state \<Rightarrow> cursor \<Rightarrow> cursor option" where
   "get_next (Ctx ((key, pres, child) # rest)) (Cursor [] prev_id) = (
     if (get_id key) \<noteq> prev_id
     then get_next (Ctx rest) (Cursor [] prev_id)
     else (
-      case get_first_diff rest prev_id of
-        Some(next_id) \<Rightarrow> Some (Cursor [] next_id)
+      case first_present rest of
+        Some next_id \<Rightarrow> Some (Cursor [] next_id)
       | None \<Rightarrow> None
     )
   )"
@@ -77,8 +81,8 @@ fun get_next :: "doc_state \<Rightarrow> cursor \<Rightarrow> cursor option" whe
 "get_next _ _ = None"
 
 fun eval_cur :: "state \<Rightarrow> expr \<Rightarrow> cursor option" where
-  "eval_cur _ Doc = Some (Cursor [] Root)"
-| "eval_cur (State doc vars) (Var x) = vars x"
+  "eval_cur state Doc = Some (Cursor [] Root)"
+| "eval_cur state (Var x) = (vars state) x"
 | "eval_cur state (Get expr key) = (
     case eval_cur state expr of
       Some (Cursor ks kn) \<Rightarrow> Some (Cursor (ks @ [MapT kn]) (Key key))
@@ -89,10 +93,17 @@ fun eval_cur :: "state \<Rightarrow> expr \<Rightarrow> cursor option" where
       Some (Cursor ks kn) \<Rightarrow> Some (Cursor (ks @ [ListT kn]) Head)
     | None \<Rightarrow> None
   )"
-| "eval_cur (State doc vars) (Next expr) = (
-    case eval_cur (State doc vars) expr of
-      Some cursor \<Rightarrow> get_next doc cursor
+| "eval_cur state (Next expr) = (
+    case eval_cur state expr of
+      Some cursor \<Rightarrow> get_next (doc state) cursor
     | None \<Rightarrow> None
+  )"
+
+fun valid_next :: "state \<Rightarrow> expr \<Rightarrow> bool" where
+  "valid_next state expr = (
+    case eval_cur state expr of
+      Some cursor \<Rightarrow> (get_next (doc state) cursor) \<noteq> None
+    | None \<Rightarrow> False
   )"
 
 inductive valid_expr :: "state \<Rightarrow> expr \<Rightarrow> bool" for state where
@@ -103,7 +114,7 @@ expr_iter: "valid_expr state expr \<Longrightarrow> valid_expr state (Iter expr)
 expr_next: "valid_expr state expr \<Longrightarrow> valid_next state expr \<Longrightarrow> valid_expr state (Next expr)"
 
 definition empty_state :: state
-where "empty_state = State (Ctx []) Map.empty"
+where "empty_state = \<lparr>doc = Ctx [], vars = Map.empty\<rparr>"
 
 lemma "valid_expr empty_state Doc"
 by (rule expr_doc)
